370963d929650097fc673a46dd2770d033992348
Jakub <113280865+NotM1NE@users.noreply.github.com>
2026-05-08 11:12:48 +0300

    Fixed directory

--- 8-uzduotis/src/io_deque.cpp
+++ 8-uzduotis/src/io_deque.cpp
@@ -12,6 +12,7 @@
 #include <ctime>
 #include <stdlib.h>
 #include <utility>
+#include <filesystem>
 
 using std::cin;
 using std::cout;
@@ -484,6 +485,7 @@ bool containsDigit(const string &str)
 
 void GenerateStudentsFile(int n)
 {
+    std::filesystem::create_directories("Data");
     srand(time(NULL));
     int ndRand = rand() % 16 + 5;
     ofstream rez("Data\\studentaiGen" + std::to_string(n) + ".txt");
