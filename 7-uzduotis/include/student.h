5ddd6f5cef5ae7a69a41c4794f3b0a1ea4c1990c
Jakub <113280865+NotM1NE@users.noreply.github.com>
2026-05-14 22:29:53 +0300

    Intialized constructor/destructor for student class

--- 7-uzduotis/include/student.h
+++ 7-uzduotis/include/student.h
@@ -19,15 +19,13 @@ private:
     double _med;
 
 public:
-    Studentas() : _egz(0), _vid(0.0), _med(0.0) {}
+    Studentas() : _vardas(""), _pavarde(""), _paz(), _egz(0), _vid(0.0), _med(0.0) {}
     Studentas(std::istream &is);
 
-    ~Studentas(); //destruktorius
-
-    Studentas(const Studentas& other); //copy konstruktorius - sukuria nauja objekta
-    Studentas& operator =(const Studentas& other); //copy priskyrimo konstruktorius - kopijojame egzistuojancia objekta
-    Studentas(Studentas && other) noexcept; //noexcept - neturetu mesti isimciu
-    Studentas& operator =(Studentas&& other) noexcept; //noexcept - neturetu mesti isimciu
+    Studentas(const Studentas &other);                // copy konstruktorius - sukuria nauja objekta
+    Studentas &operator=(const Studentas &other);     // copy priskyrimo konstruktorius - kopijojame egzistuojancia objekta
+    Studentas(Studentas &&other) noexcept;            // noexcept - neturetu mesti isimciu
+    Studentas &operator=(Studentas &&other) noexcept; // noexcept - neturetu mesti isimciu
 
     inline const string vardas() const { return _vardas; }
     inline const string pavarde() const { return _pavarde; }
@@ -36,20 +34,28 @@ public:
     inline double vid() const { return _vid; }
     inline double med() const { return _med; }
 
-    std::istream &ReadStudent(std::istream&);
+    std::istream &ReadStudent(std::istream &);
 
-    void SetVardas(const string& vardas) { _vardas = vardas; }
-    void SetPavarde(const string& pavarde) { _pavarde = pavarde; }
+    void SetVardas(const string &vardas) { _vardas = vardas; }
+    void SetPavarde(const string &pavarde) { _pavarde = pavarde; }
     void SetEgz(int egz) { _egz = egz; }
     void SetVid(double vid) { _vid = vid; }
     void SetMed(double med) { _med = med; }
-    void AddPaz(int paz) {_paz.push_back(paz); }
+    void AddPaz(int paz) { _paz.push_back(paz); }
     void ClearPaz() { _paz.clear(); }
 
     void MedIrVidSkaciavimas(int sum);
+
+    // destruktorius
+    ~Studentas()
+    {
+        _vardas.clear();
+        _pavarde.clear();
+        _paz.clear();
+    }
 };
 
-std::istream& operator>>(std::istream& is, Studentas& s);
-std::ostream& operator<<(std::ostream& os, const Studentas& s);
+std::istream &operator>>(std::istream &is, Studentas &s);
+std::ostream &operator<<(std::ostream &os, const Studentas &s);
 
 #endif
