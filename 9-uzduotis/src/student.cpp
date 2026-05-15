5e101f8fb3bd5c702e92b859cc2aaf6679125ed3
Jakub <113280865+NotM1NE@users.noreply.github.com>
2026-05-15 09:23:05 +0300

    fixed destructor

--- 9-uzduotis/src/student.cpp
+++ 9-uzduotis/src/student.cpp
@@ -19,14 +19,6 @@ std::istream &Studentas::ReadStudent(std::istream &is)
     is >> _vardas >> _pavarde;
     return is;
 }
-//Destruktorius
-Studentas::~Studentas()
-{
-    _paz.clear();
-    _egz = 0;
-    _vid = 0.0;
-    _med = 0.0;
-}
 
 //Copy konstruktorius
 Studentas::Studentas(const Studentas &other)
